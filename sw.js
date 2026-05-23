ab0410427b931481cced8f7150ef25bd8a04acf0
uknowaz316 <389405641@qq.com>
2026-05-23 10:15:00 +0800

    初始提交

---
+++ sw.js
@@ -0,0 +1,39 @@
+// service worker - 离线缓存支持
+const CACHE_NAME = 'biz-tracker-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './1.png',
+  './2.png',
+  './3.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
